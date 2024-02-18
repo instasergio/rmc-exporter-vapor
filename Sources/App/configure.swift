dbfcd4af87a99daeb2ccb262ca9bafad91b27a7f
instasergio <instasergio@ya.ru>
2024-02-18 16:50:15 +0300

    fixes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,9 +2,9 @@ import Vapor
 import NIOSSL
 
 public func configure(_ app: Application) throws {
-    #if DEBUG
+    
     app.logger.logLevel = .debug
-    #endif
+
 
 //    app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
 //        certificateChain: try NIOSSLCertificate.fromPEMFile("../../fullchain.pem").map { .certificate($0) },
